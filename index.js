7a980f0fed8eb34695be0ac08f56521d6fa802eb
jakezappin <jake.zappin@gmail.com>
2016-11-07 10:11:09 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+
+function map(collection, callback) {
+  const result = [];
+
+  for (var i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    result.push(callback(element));
+  }
+
+  return result;
+}
